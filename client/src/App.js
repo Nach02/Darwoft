8889a0cd5a617a35b6e138a9b6e2b3c0f3b4301f
ignacio martinez <ignacio.martinez.i@hotmail.com>
2022-05-23 16:51:14 -0300

    first

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Redux/Reducer.js
@@ -0,0 +1,12 @@
+import {} from './Actions'
+
+const initialState={  
+}
+
+const RootReducer=(state=initialState,action)=>{
+    switch(action.type){ 
+        
+        default: return state;
+    }
+}
+export default RootReducer
